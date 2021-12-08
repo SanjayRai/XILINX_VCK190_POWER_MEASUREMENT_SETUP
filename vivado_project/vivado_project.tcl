2a5ffd357210e42c10c3f3e9f288eaa3906cac30
Sanjay Rai <sanjay.d.rai@gmail.com>
2021-12-08 00:42:55 -0500

    Added SImulation  SAIF and  report_power with SAIF file

--- vivado_project/vivado_project.tcl
+++ vivado_project/vivado_project.tcl
@@ -20,7 +20,25 @@ add_files -fileset sources_1 -norecurse {
 }
 add_files -fileset constrs_1 -norecurse ../xdc/top.xdc
 
-set_property top vck190_tester_top [current_fileset]
+set_property SOURCE_SET sources_1 [get_filesets sim_1]
+add_files -fileset sim_1 -norecurse ../rtl/tb_vck190_tester_top.sv
+update_compile_order -fileset sim_1
+set_property top tb_vck190_tester_top [get_filesets sim_1]
+set_property top_lib xil_defaultlib [get_filesets sim_1]
+set_property -name {xsim.simulate.runtime} -value {0} -objects [get_filesets sim_1]
+update_compile_order -fileset sim_1
+
+proc generate_saif {} {
+launch_simulation
+restart
+open_saif  ./VCK190_test.saif
+log_saif /tb_vck190_tester_top/UUT/u_power_consumer_top/*
+run 100 ns
+close_saif
+}
+
+proc run_build {} {
+set_property top vck190_tester_top [get_filesets sources_1]
 launch_runs synth_1 -jobs 8
 wait_on_run synth_1
 launch_runs impl_1 -jobs 8
@@ -29,7 +47,9 @@ open_run impl_1
 launch_runs impl_1 -to_step write_device_image -jobs 8
 wait_on_run impl_1
 write_hw_platform -fixed -include_bit -force -file ./vck190_tester_top.xsa
+read_saif ./VCK190_test.saif
 report_power -file {./vck190_tester_top_power.txt} -xpe {./vck190_tester_top_power.xpe} -rpx {./vck190_tester_top_power.rpx} -name {power_1}
 report_utilization -file {./vck190_tester_top_utilization.txt} -name utilization_1
 report_timing_summary -delay_type min_max -report_unconstrained -check_timing_verbose -max_paths 10 -input_pins -routable_nets -name timing_1 -file ./vck190_tester_top_timing_summary.txt
 report_clock_interaction -delay_type min_max -significant_digits 3 -name timing_2
+}
